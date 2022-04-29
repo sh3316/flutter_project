688c6345e0d9d4472d7ff4b47a065c3bc35cefe2
sh3316 <chuh2016@gmail.com>
2022-04-29 23:08:19 +0900

    당근마켓

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,35 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title:Text('앱임')),
-        body: SizedBox()
+        appBar: AppBar(backgroundColor: Colors.white,
+          leadingWidth: 70,
+          leading: Row(crossAxisAlignment:CrossAxisAlignment.center,
+              children:
+              [
+                SizedBox(width: 20),
+                Text('금호동 3가', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                Icon(Icons.arrow_drop_down, color: Colors.black,),
+              ]
+          ),
+          actions: [
+            Icon(Icons.search, color: Colors.black,size: 30,),
+            SizedBox(width: 20,),
+            Icon(Icons.menu, color: Colors.black,size: 30,),
+            SizedBox(width: 20,),
+            Icon(Icons.doorbell, color: Colors.black,size: 30,),
+            SizedBox(width: 10,)
+          ],
+        ),
+        body: Column(
+          children: [
+            Row(children: [
+              Container(
+                child: Image.asset('camera.png',width: 100, height: 100), padding: EdgeInsets.only(left: 2, right: 2, bottom: 1)
+              )
+            ],),
+            Row(children: [],)
+          ]
+        )
     ));
   }
 }
